e14596df00372fd8a89c7332cdd651c0728f9339
Tyler Collier <tyler.collier+github@devmounta.in>
2017-10-16 00:00:15 -0700

    A solution using Auth0's Lock UI library

--- src/App.js
+++ src/App.js
@@ -1,9 +1,59 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Auth0Lock from 'auth0-lock';
+import axios from 'axios';
 
 class App extends Component {
+  state = {
+    user: null,
+    secureDataResponse: null
+  };
+  lock = null;
+
+  componentDidMount() {
+    this.lock = new Auth0Lock(process.env.REACT_APP_AUTH0_CLIENT_ID, process.env.REACT_APP_AUTH0_DOMAIN);
+    this.lock.on('authenticated', this.onAuthenticated);
+    axios.get('/user-data').then(response => {
+      this.setState({ user: response.data.user || null });
+    });
+  }
+
+  onAuthenticated = (authResult) => {
+    console.log('authResult', authResult);
+    axios.post('/login', { idToken: authResult.idToken} ).then(response => {
+      this.setState({ user: response.data.user });
+    })
+  };
+
+  login = () => {
+    this.lock.show();
+  };
+
+  logout = () => {
+    axios.post('/logout').then(() => {
+      this.setState({ user: null });
+    });
+  };
+
+  getMessage = error => error.response
+    ? error.response.data
+      ? error.response.data.message
+      : JSON.stringify(error.response.data, null, 2)
+    : error.message;
+
+  fetchSecureData = () => {
+    axios.get('/secure-data').then(response => {
+      this.setState({ secureDataResponse: JSON.stringify(response.data, null, 2) });
+    }).catch(error => {
+      this.setState({ secureDataResponse: this.getMessage(error) });
+    })
+  };
+
   render() {
+    const { user, secureDataResponse } = this.state;
+    const userData = JSON.stringify(user, null, 2);
+
     return (
       <div className="App">
         <header className="App-header">
@@ -11,7 +61,19 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <div className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <div className="section">
+            <button onClick={this.login}>Log in</button>
+            {' '}
+            <button onClick={this.logout}>Log out</button>
+          </div>
+          <div className="section">
+            <h2>User data:</h2>
+            <div>{userData}</div>
+          </div>
+          <div className="section">
+            <button onClick={this.fetchSecureData}>Fetch secure data</button>
+            <div>{secureDataResponse}</div>
+          </div>
         </div>
       </div>
     );
